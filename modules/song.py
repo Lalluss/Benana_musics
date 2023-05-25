53b1922c370e397a6e675edaf493409e90c060df
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-05-25 11:15:20 +0530

    Update song.py

--- modules/song.py
+++ modules/song.py
@@ -88,7 +88,7 @@ def a(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep ='<a>{title}</a>\n\n❍ <b>Duration:</b> <code>{duration}</code>\n❍ <b>Uploaded By:</b> <a href="https://t.me/Edit_Repo">BenbotZ</a>\n<b>❍ Source</b>:<a href="{link}">click here</a>'
+        rep ='<a href="{link}">{title}</a>\n\n❍ <b>Duration:</b> <code>{duration}</code>\n❍ <b>Uploaded By:</b> <a href="https://t.me/Edit_Repo">BenbotZ</a>'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)

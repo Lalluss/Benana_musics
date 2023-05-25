2b45a442cb8f93955e0201fae82fa485cd4b7caf
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-05-25 12:09:00 +0530

    Update song.py

--- modules/song.py
+++ modules/song.py
@@ -89,8 +89,8 @@ def a(client, message):
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
             secmul *= 60
-        message.reply_audio(audio_file, caption=rep, parse_mode='HTML',quote=False, title=title, duration=dur, performer=performer, thumb=thumb_name)
-        reply_to_message_id=message.message_id
+        s=message.reply_audio(audio_file, caption=rep, parse_mode='HTML',quote=False, title=title, duration=dur, performer=performer, thumb=thumb_name)
+        reply_to_message_id=s.message_id
         m.delete()
     except Exception as e:
         m.edit('**An internal Error Occured, Report This @Edit_repo !!**')

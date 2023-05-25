7a90133e6d7ff1ae98c342a2c4a2895b6348c6f1
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-05-25 10:31:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class Config:
     APP_ID = int(os.environ.get("APP_ID", "7111812"))
     API_HASH = os.environ.get("API_HASH", "064880c569c803881b46d65969452ddf")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6027561066:AAEY3zfQT3vf86UdEojodvq1rgvOmajtJZI")
-    START_MSG = os.environ.get("START_MSG", "<b>Hi {},\nIam A Simple Youtube to Mp3 Downloader Bot,</b>\n\nSend me Any Songs name with /song command")
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2a35fca576aa49de77c98.jpg")
+    START_MSG = os.environ.get("START_MSG", "<b>Hi {},\nIam A Song Downloader I am only Work MusicSiena Group Click The Group Btn And join group and ask songs with command there\n\n/song - song name")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/3ca56321b6b3ccaa84929.jpg")
     OWNER = os.environ.get("OWNER", "lallus_tg") 
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
     msg = {}

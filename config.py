7694a96f67b017fb88a2c2270ea1751ac07d7a97
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-05-25 10:14:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import re
 from youtube_dl import YoutubeDL
 
 class Config:
-    APP_ID = int(os.environ.get("APP_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    APP_ID = int(os.environ.get("APP_ID", "7111812"))
+    API_HASH = os.environ.get("API_HASH", "064880c569c803881b46d65969452ddf")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6027561066:AAEY3zfQT3vf86UdEojodvq1rgvOmajtJZI")
     START_MSG = os.environ.get("START_MSG", "<b>Hi {},\nIam A Simple Youtube to Mp3 Downloader Bot,</b>\n\nSend me Any Songs name with /song command")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2a35fca576aa49de77c98.jpg")
-    OWNER = os.environ.get("OWNER", "shamilhabeeb") 
+    OWNER = os.environ.get("OWNER", "lallus_tg") 
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
     msg = {}

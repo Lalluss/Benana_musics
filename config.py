2111f26b8359766faef6b4a60c64238226e2a5b1
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-05-26 15:45:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     APP_ID = int(os.environ.get("APP_ID", "7111812"))
     API_HASH = os.environ.get("API_HASH", "064880c569c803881b46d65969452ddf")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6027561066:AAEY3zfQT3vf86UdEojodvq1rgvOmajtJZI")
-    START_MSG = os.environ.get("START_MSG", "<b>Hey {}🍁,\n\nMy name is Shinobu Kocho i'm a simple and advanced music bot made for MusicSeina Group")
+    START_MSG = os.environ.get("START_MSG", "<b>Hey {}🍁,\n\nMy name is Mɪᴏɴ i'm a simple and advanced music bot made for MusicSeina Group")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/02214a2967c30c4108bc8.jpg")
     OWNER = os.environ.get("OWNER", "lallus_tg") 
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")

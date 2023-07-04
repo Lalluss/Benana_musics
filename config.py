a93d0dc874a338acc4e15a740ee042e0427560d5
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-07-04 21:43:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH", " ")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", " ")
     START_MSG = os.environ.get("START_MSG", "<b>Hey {}🍁,\n\n𝗠𝘆 𝗡𝗮𝗺𝗲 𝗜𝘀 Mɪᴏɴ 𝗜'𝗺 𝗔 𝗦𝗶𝗺𝗽𝗹𝗲 𝗔𝗻𝗱 𝗔𝗱𝘃𝗮𝗻𝗰𝗲𝗱 𝗠𝘂𝘀𝗶𝗰 𝗕𝗼𝘁 𝗠𝗮𝗱𝗲 𝗙𝗼𝗿 MᴜsɪᴄSᴇɪɴᴀ Gʀᴏᴜᴘ")
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/02214a2967c30c4108bc8.jpg")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/68b004463396b09ebb3b3.jpg")
     OWNER = os.environ.get("OWNER", " ") 
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
     msg = {}

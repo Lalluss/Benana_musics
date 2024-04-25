89787f76d7da877b12b6d3726351c1d2affe1e2a
Lalluss <123368967+Lalluss@users.noreply.github.com>
2024-04-25 16:05:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ class cust(object):
     B_NAME = None
 
 class Config:
-    APP_ID = int(os.environ.get("APP_ID", " "))
-    API_HASH = os.environ.get("API_HASH", " ")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", " ")
+    APP_ID = int(os.environ.get("APP_ID", "7111812"))
+    API_HASH = os.environ.get("API_HASH", "064880c569c803881b46d65969452ddf")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6985438359:AAGkbDefY73myVSMMY5zK15YEPOxVF-wepU")
     START_MSG = os.environ.get("START_MSG", "<b>Hey {}🍁,</b>\n\n<b>My Name Is</b> 𝐌𝐈𝐎𝐍 <b>I am A simple Music Downloader Bot Am Only Work In Groups Just Add Me To Your Group And Use /song command</b>")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/3357581ba3d7be61ac672.jpg")
     OWNER = os.environ.get("OWNER", " ") 

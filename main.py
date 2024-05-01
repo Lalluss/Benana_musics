9453ef1692c37707d1154f42c3646c26bf4b6383
Lalluss <123368967+Lalluss@users.noreply.github.com>
2024-05-01 14:06:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ from plugins import web_server
 PORT = environ.get("PORT", "8080")
 
 app = web.AppRunner
-await web_server()
 await app.setup()
 bind_address = "0.0.0.0"
 await web.TCPSite(app, bind_address, PORT).start()

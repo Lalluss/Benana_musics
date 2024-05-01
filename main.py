0260f5ac558b4689a8b935dbc98e216b4519549a
Lalluss <123368967+Lalluss@users.noreply.github.com>
2024-05-01 22:02:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-import os
-from os import environ
+
 
 from pyrogram.raw.all import layer
 
@@ -20,14 +18,6 @@ from config import Config
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from aiohttp import web
-from modules import web_server
-PORT = environ.get("PORT", "8080")
-
-app = web.AppRunner(web_server())
-app.setup()
-bind_address = "0.0.0.0"
-web.TCPSite(app, bind_address, PORT).start()
 
 app = pyrogram.Client(
       "mlz",

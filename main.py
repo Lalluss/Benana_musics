dc342b31474998b091da34c0c437fb6950455dfe
Lalluss <123368967+Lalluss@users.noreply.github.com>
2024-05-01 14:28:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from aiohttp import web
 from plugins import web_server
 PORT = environ.get("PORT", "8080")
 
-app = web.AppRunner
+app = web.AppRunner(await web_server())
 await app.setup()
 bind_address = "0.0.0.0"
 await web.TCPSite(app, bind_address, PORT).start()

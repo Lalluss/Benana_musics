0b50d683d63e22961303cfceb61ada2e8d26413a
Lalluss <123368967+Lalluss@users.noreply.github.com>
2024-05-05 19:05:23 +0530

    Update song.py

--- modules/song.py
+++ modules/song.py
@@ -91,7 +91,7 @@ async def song_fetch(client, message):
             ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
             # if time_to_seconds(duration) >= 7000:  # duration limit
             #     m.edit("Exceeded 30mins cap")
-            #     return
+            #     retun
 
             performer = f"[@AnnabenbotZ]"
             thumb_name = f'thumb{message.id}.jpg'
@@ -112,6 +112,7 @@ async def song_fetch(client, message):
         )
         print(str(e))
         return
+    d=await message.reply_text("`✨ Fetching... `")
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
@@ -147,6 +148,7 @@ async def song_fetch(client, message):
                 InlineKeyboardButton("Can't Access? Click Here", url="https://t.me/+llHTG1lBN8E2ZmQ9")
             ]
         ]
+        await d.delete()
         crazymsg = await msg.reply_text(text=CRAZY_TXT, reply_markup=reply_markup)
     except Exception as e:
         print(f"{e}")

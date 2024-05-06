c87f9c301b969c7ac12000f15b1ec088489a9bd0
Lalluss <123368967+Lalluss@users.noreply.github.com>
2024-05-06 05:32:07 +0530

    Update song.py

--- modules/song.py
+++ modules/song.py
@@ -141,16 +141,16 @@ async def song_fetch(client, message):
             thumb=thumb_name,
         )
         await d.delete
-            btn = [[
-                InlineKeyboardButton("♻️DOWNLOAD♻️", callback_data='crazy.link')
-                ],[
-                InlineKeyboardButton("🔰JOIN CHANNEL🔰", url='https://t.me/+j4kOYjoPw9dmMDll')
-            ]]
-            reply_markup = InlineKeyboardMarkup(btn)
-            bb = await query.message.reply_text(
-                text = f"Hi click the below link and download the movies🍿\n\nERROR? Click the join channel button and try again \n\n{crazy.link}",
-                reply_markup = reply_markup
-            )
+        btn = [[
+            InlineKeyboardButton("♻️DOWNLOAD♻️", callback_data='crazy.link')
+            ],[
+            InlineKeyboardButton("🔰JOIN CHANNEL🔰", url='https://t.me/+j4kOYjoPw9dmMDll')
+        ]]
+        reply_markup = InlineKeyboardMarkup(btn)
+        await query.message.reply_text(
+            text = f"Hi click the below link and download the movies🍿\n\nERROR? Click the join channel button and try again \n\n{crazy.link}",
+            reply_markup = reply_markup
+        )
     except Exception as e:
         print(f"{e}")
     

1ea2fb952d99500d53a6215693a13b3969bc773a
Lalluss <123368967+Lalluss@users.noreply.github.com>
2024-05-08 15:26:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class cust(object):
 class Config:
     APP_ID = int(os.environ.get("APP_ID", "7111812"))
     API_HASH = os.environ.get("API_HASH", "064880c569c803881b46d65969452ddf")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6985438359:AAGkbDefY73myVSMMY5zK15YEPOxVF-wepU")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "music_2_0_bot")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6383975910:AAGCu6AKfrBc8Ptyqi22KW3ZQr8mBhhNpNA")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Music_S_eina_bot")
     START_MSG = os.environ.get("START_MSG", "<b>Hey {}🍁,</b>\n\n<b>My Name Is</b> ѦηηѦ ɓ℮η <b>I am A simple Music Downloader Bot Am Only Work In Groups Just Add Me To Your Group And Use /song command</b>")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/895e4c2ad6766acabedb1.jpg")
     OWNER = os.environ.get("OWNER", " ") 

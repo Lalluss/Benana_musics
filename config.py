fd691456aaf4bde60c7686041f1a5ed5bac23a9c
Lalluss <123368967+Lalluss@users.noreply.github.com>
2024-05-10 10:14:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "064880c569c803881b46d65969452ddf")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6383975910:AAGCu6AKfrBc8Ptyqi22KW3ZQr8mBhhNpNA")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "Music_S_eina_bot")
-    START_MSG = os.environ.get("START_MSG", "<b>Hey {}🍁,</b>\n\n<b>My Name Is</b> ѦηηѦ ɓ℮η <b>I am A simple Music Downloader Bot Am Only Work In Groups Just Add Me To Your Group And Use /song command</b>")
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/e5c1339f8b4f234bc178a.jpg")
+    START_MSG = os.environ.get("START_MSG", "<b>Hey {}🍁,</b>\n\n<b>My Name Is</b> ᴀɴɢᴀʟɪɴᴇ <b>I am A simple Music Downloader Bot Am Only Work In Groups Just Add Me To Your Group And Use /song command</b>")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/a5a1a5ceda7dd4fb3e6de.jpg")
     OWNER = os.environ.get("OWNER", " ") 
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
     msg = {}

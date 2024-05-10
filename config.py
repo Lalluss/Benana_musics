487c7736eac40a2d2f6a603e414079a65909352c
Lalluss <123368967+Lalluss@users.noreply.github.com>
2024-05-10 09:53:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6383975910:AAGCu6AKfrBc8Ptyqi22KW3ZQr8mBhhNpNA")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "Music_S_eina_bot")
     START_MSG = os.environ.get("START_MSG", "<b>Hey {}🍁,</b>\n\n<b>My Name Is</b> ѦηηѦ ɓ℮η <b>I am A simple Music Downloader Bot Am Only Work In Groups Just Add Me To Your Group And Use /song command</b>")
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/4ba4a9a9a5d26a33fe435.jpg")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/e5c1339f8b4f234bc178a.jpg")
     OWNER = os.environ.get("OWNER", " ") 
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
     msg = {}

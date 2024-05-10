0d77495c0513feb189e473dee0667998b6cd8fba
Lalluss <123368967+Lalluss@users.noreply.github.com>
2024-05-10 19:08:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "Music_S_eina_bot")
     START_MSG = os.environ.get("START_MSG", "<b>Hey {}🍁,</b>\n\n<b>My Name Is</b> ᴀɴɢᴀʟɪɴᴇ <b>I am A simple Music Downloader Bot Am Only Work In Groups Just Add Me To Your Group And Use /song command</b>")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/a5a1a5ceda7dd4fb3e6de.jpg")
-    HELP_MSG = os.environ.get("HELP_MSG", "Hello")
+    HELP_MSG = os.environ.get("HELP_MSG", "👋ʜᴇy its very easy to request music here\n\nʀᴇqᴜᴇꜱᴛ - ᴇxᴀᴍᴩʟᴇꜱ:\n<code>➲ /song Srivalli Malayalam</code>\n➲ <code>/song Darshana hridayam</code>\n➲ <code>/song Alone - Marshmallow</code>\n<code>➲ /song Aathmavile anandhame</code>\n➲ <code>/song Parayathe vannnen</code>\n\nʜᴏᴩᴇ yᴏᴜ ᴜɴᴅᴇʀꜱᴛᴏᴏᴅ 😌🍭')")
     OWNER = os.environ.get("OWNER", " ") 
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
     msg = {}

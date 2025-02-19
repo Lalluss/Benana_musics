70872c2dfc6b5727573f7ac13d12323dddb53400
Lalluss <123368967+Lalluss@users.noreply.github.com>
2025-02-19 13:55:40 +0530

    Thyr

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class Lallus(Client):
 
     def __init__(self):
         super().__init__(
-            api_id=Config.API_ID,
+            api_id=Config.APP_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
             workers=50,

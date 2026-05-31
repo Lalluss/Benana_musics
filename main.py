268ccedd1ad7e30534faeb9e0095932cc31ca309
Lalluss <123368967+Lalluss@users.noreply.github.com>
2026-05-31 12:01:26 +0530

    Clean up imports and adjust logging level
    Refactor logging configuration and import statements.

--- main.py
+++ main.py
@@ -1,33 +1,10 @@
-import re
-from os import environ
-
-from pyrogram.raw.all import layer
-
-from pyrogram import Client, filters
-
-import youtube_dl
-from youtube_search import YoutubeSearch
-import requests
-
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
 import os
+from pyrogram import Client
 from config import Config
+import logging
 
-import pyrogram
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-
-from aiohttp import web
-from Lallus import web_server
-PORT = environ.get("PORT", "8080")
-
-app = Client("SESSION",
-api_id=Config.APP_ID, 
-api_hash=Config.API_HASH, 
-bot_token=Config.BOT_TOKEN)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class Lallus(Client):
     def __init__(self):
@@ -40,11 +17,6 @@ class Lallus(Client):
             plugins={"root": "modules"},
             sleep_threshold=5,
         )
-    async def start(self):
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
 
 bot = Lallus()
 bot.run()
